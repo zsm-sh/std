34d985aaf82fa6291dfcb6acc7946ce1ee3be6df
Shiming Zhang <wzshiming@foxmail.com>
2022-03-24 20:10:29 +0800

    Fix arch

--- src/runtime/arch.sh
+++ src/runtime/arch.sh
@@ -9,14 +9,14 @@ function runtime::arch() {
     x86_64 | amd64 | x64)
         echo "amd64"
         ;;
-    i686 | i386 | x86)
+    i386 | i486 | i586 | i686 | x86)
         echo "386"
         ;;
-    armv7l | armv8l | armhf)
-        echo "arm"
-        ;;
-    aarch64 | arm64)
+    armv8* | aarch64* | arm64)
         echo "arm64"
+        ;;
+    armv7* | armhf)
+        echo "arm"
         ;;
     *)
         echo "${arch}"
